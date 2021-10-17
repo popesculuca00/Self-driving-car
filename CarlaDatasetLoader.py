c3580c837753cbc2b2454fde2ab3e32031a2432d
popesculuca00 <lucapopescu.lp@gmail.com>
2021-10-17 23:10:57 +0300

    data-pipeline

--- CarlaDatasetLoader.py
+++ CarlaDatasetLoader.py
@@ -3,48 +3,28 @@ import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 import glob
 from imgaug import augmenters as iaa
-from torchvision.transforms.transforms import ColorJitter
+from torchvision.transforms import ColorJitter
 import h5py
 import numpy as np
 from torchvision.io import read_image
 import pandas as pd
 import torch
+from PIL import Image
+import matplotlib.pyplot as plt
 
 class CarlaDataset(Dataset):
-    def __init__(self, target_dir, is_train=True):
+    def __init__(self, target_dir, for_training=True):
         #super(CarlaDataset, self).__init__()
-        csv = pd.read_csv(target_dir)[:20]
+        csv = pd.read_csv(target_dir)[:40]
         self.speeds = csv["Speed"].values
         self.targets = csv[["Steer", "Gas", "Brake"]].values
         self.imgs = csv["Target path"].values
         self.commands = csv["High level command"].values
-        self.is_train = is_train
-        
-        self.init_transforms()
-    
-    def init_transforms(self):
-        if self.is_train:
-
-            seq = iaa.Sequential([
-                iaa.Sometimes( 0.5, iaa.GaussianBlur((0.0, 1.5)) ),
-                iaa.Sometimes( 0.5, iaa.AdditiveGaussianNoise(scale=(0.0, 2))),
-                iaa.OneOf([ iaa.Dropout( (0.0, 1.0), per_channel=0.05),
-                            iaa.CoarseDropout(0.005, size_percent=0.1)])
-            ])
-
-            self.transforms = transforms.Compose([
-                transforms.Lambda(lambda x: seq(x))
-            ])
-        else:
-            self.transforms = transforms.Compose([
-                transforms.ToTensor()
-                ])
-
+        self.trsfs = get_transforms(for_training)
 
     def __getitem__(self, index):
-
-        img = read_image(self.imgs[index])
-        img = self.transforms(img)
+        img_orig = np.asarray( Image.open(self.imgs[index]), dtype=np.float32)
+        img = self.trsfs(img_orig)
         target = self.targets[index]
         command = int(self.commands[index]-2)
         target_vec = np.zeros((4,3), dtype=np.float32 )
@@ -54,29 +34,40 @@ class CarlaDataset(Dataset):
         mask_vec = np.zeros((4,3), dtype= np.float32)
         mask_vec[command, :] = np.ones((1,3), dtype=np.float32 )
 
-        return img, speed, target_vec, mask_vec#.reshape(-1),
+        return img, speed, target_vec, mask_vec, img_orig#.reshape(-1),
 
     
     def __len__(self):
         return len(self.imgs)
 
-def get_train_augmenters():
-    aug = iaa.Sequential([
-        
-    ])
-
-
-def get_dataloader( target_dir = "data\data.csv", num_workers=8, batch_size=32, is_train=True):
-    dataset = CarlaDataset(target_dir, is_train=is_train)
-
-    if is_train:
-        image_augmenters = get_train_augmenters()
+def get_transforms(for_training=True):
     
-    
-    dataloader = DataLoader(dataset, augmenters=image_augmenters, num_workers=num_workers, batch_size=batch_size, shuffle=True)
+    if for_training:
+        trsfs = transforms.Compose([
+                iaa.Sequential([
+                        iaa.Sometimes(0.15, [iaa.GaussianBlur((0.0, 0.75))] ),
+                        iaa.Sometimes(0.2, [iaa.AdditiveGaussianNoise(scale=(0.0, 0.02))]),
+                        iaa.Sometimes( 0.4,[
+                            iaa.OneOf([
+                                iaa.Dropout( (0.0, 0.05), per_channel=0.01) ,
+                                iaa.CoarseDropout((0.0,0.1), size_percent=(0.08, 0.2), per_channel=0.5) ])] ) 
+                        ]).augment_image,
+                transforms.ToTensor()
 
-                #     iaa.GaussianBlur((0.0, 1.5)),
-                #     iaa.AdditiveGaussianNoise(scale=(0.0, 0.05)),
-                #     iaa.Dropout( (0.0, 1.0), per_channel=0.05) ,
-                #     iaa.CoarseDropout(0.005, size_percent=0.1)]),
-                # transforms.ColorJitter(brightness= 0.35 ),
+        ])
+           
+                #
+    else:
+        trsfs = transforms.Compose([
+                    transforms.ToTensor()
+        ])
+    return trsfs
+
+if __name__ == "__main__":
+    ds = CarlaDataset("data\data.csv", for_training=True)
+    dl = DataLoader(dataset =ds, num_workers=0, batch_size=2)
+    for index, (img, speed, target_vec, mask_vec, img_orig) in enumerate(dl):
+        pass
+    print(img.shape)
+    img = img[1].permute(  1, 2, 0)
+    plt.imshow(img.numpy()/255)
