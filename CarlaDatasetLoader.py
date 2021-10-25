acd7af6bff90a5b7c14b55db38adcf836a28ef25
popesculuca00 <lucapopescu.lp@gmail.com>
2021-10-25 16:37:52 +0300

    fixed real-only array problems

--- CarlaDatasetLoader.py
+++ CarlaDatasetLoader.py
@@ -21,7 +21,7 @@ class CarlaBaseDataset(Dataset):
         self.commands = csv["High level command"].values
 
     def __getitem__(self, index):
-        img = np.array( Image.open(self.imgs[index]), dtype=np.uint8, copy=True) #convert to uint8 for gaussian noise
+        img = np.array( Image.open(self.imgs[index]), dtype=np.uint8, copy=True) #convert to uint8 for gaussian noise, copy= True for warning suppression
         target = self.targets[index]
         command = int(self.commands[index]-2)
         target_vec = np.zeros((4,3), dtype=np.float32 )
